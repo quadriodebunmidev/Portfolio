3892cb93e61c9e3fe20859180c63c69c9ba0ab84
quadriodebunmidev <quadriportfolio@gmail.com>
2026-07-04 12:33:56 +0100

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,8 +2,8 @@ import { useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import About from '../components/About';
-import Skills from '../components/Skills';
-import Projects from '../components/Projects';
+//import Skills from '../components/Skills';
+//import Projects from '../components/Projects';
 //import Experience from '../components/Experience';
 import Certificates from '../components/Certificates';
 import Testimonials from '../components/Testimonials';
@@ -19,9 +19,9 @@ export default function Home() {
       <Navbar />
       <Hero />
       <About />
-      <Skills /> 
+      {/*  <Skills /> 
       <Projects />
-      {/*  <Experience />*/}
+        <Experience />*/}
       <Certificates />
       <Testimonials />
       <Contact />
